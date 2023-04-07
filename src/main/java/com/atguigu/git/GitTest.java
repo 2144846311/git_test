9495e3b63049ec419b3ef5a8c23b782c1eecff24
admin <2144846311@qq.com>
2023-04-07 11:41:28 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author wjcsrart
+ * @create 2023-04-07 11:39
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello，git!");
+    }
+}
